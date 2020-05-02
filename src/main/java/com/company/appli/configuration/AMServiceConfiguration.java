a094303c410fe5708a3115dac08d440dee984160
Philippe <philippej13@yahoo.fr>
2020-05-02 03:40:53 +0200

    Add Actuator with @RefreshScope

--- src/main/java/com/company/appli/configuration/AMServiceConfiguration.java
+++ src/main/java/com/company/appli/configuration/AMServiceConfiguration.java
@@ -1,9 +1,11 @@
 package com.company.appli.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RefreshScope
 public class AMServiceConfiguration {
     @Value("${am.management.baseUri}")
     private String amManagementBaseUri;
